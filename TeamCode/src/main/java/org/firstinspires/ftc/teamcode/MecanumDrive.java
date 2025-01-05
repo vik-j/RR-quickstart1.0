3b8ef105a90bcd56ab56899389944ddbc122ca8b
vik-j <114836259+vik-j@users.noreply.github.com>
2025-01-04 18:02:11 -0800

    Update MecanumDrive.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -83,9 +83,9 @@ public final class MecanumDrive {
         public double maxAngAccel = Math.PI;
 
         // path controller gains
-        public double axialGain = 7;
-        public double lateralGain = 5;
-        public double headingGain = 7; // shared with turn
+        public double axialGain = 10;
+        public double lateralGain = 7;
+        public double headingGain = 8; // shared with turn
 
         public double axialVelGain = 0.0;
         public double lateralVelGain = 0.0;
