6a31f0743b205c229e2c0f50b4a1b629d5047a5c
MaximeSobrier <142960631+MaximeSobrier@users.noreply.github.com>
2025-01-30 19:06:19 -0800

    Update SpeciSideAutoFiveSpeci.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/newAuto/SpeciSideAutoFiveSpeci.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/newAuto/SpeciSideAutoFiveSpeci.java
@@ -158,7 +158,7 @@ public class SpeciSideAutoFiveSpeci extends LinearOpMode {
                 //TODO: pickup 2nd speci
                 .strafeToLinearHeading(new Vector2d(-35.52, 47), Math.toRadians(-90))
                 .waitSeconds(0.5)
-                .strafeToLinearHeading(new Vector2d(-35.52, 53), Math.toRadians(-90))
+                .strafeToLinearHeading(new Vector2d(-35.52, 53.5), Math.toRadians(-90))
                 .afterTime(0.35, telemetryPacket -> {
                     bot.grippyClose();
                     return false;
@@ -267,13 +267,12 @@ public class SpeciSideAutoFiveSpeci extends LinearOpMode {
                     bot.flippy.setPosition(0.9);
                     return false;
                 })
-                .afterTime(1, telemetryPacket -> {
+                .afterTime(1.15, telemetryPacket -> {
                     bot.specimenDeposit();
                     return false;
                 })
                 //TODO: drop off 4th speci
-                .strafeToConstantHeading(new Vector2d(6, 35.5))
-
+                .strafeToConstantHeading(new Vector2d(7, 35.5))
                 .afterTime(0.5, telemetryPacket -> {
                     bot.specimenDeposit2();
                     return false;
@@ -282,7 +281,7 @@ public class SpeciSideAutoFiveSpeci extends LinearOpMode {
                     bot.grippyOpen();
                     return false;
                 })
-                .waitSeconds(0.3)
+                .waitSeconds(0.2)
                 .afterTime(0.75, telemetryPacket -> {
                     bot.specimenPickup();
                     return false;
@@ -310,13 +309,12 @@ public class SpeciSideAutoFiveSpeci extends LinearOpMode {
                     bot.flippy.setPosition(0.9);
                     return false;
                 })
-                .afterTime(1, telemetryPacket -> {
+                .afterTime(1.15, telemetryPacket -> {
                     bot.specimenDeposit();
                     return false;
                 })
                 //TODO: drop off 5th speci
-                .strafeToConstantHeading(new Vector2d(7.5, 35.5))
-
+                .strafeToConstantHeading(new Vector2d(7, 35.5))
                 .afterTime(0.5, telemetryPacket -> {
                     bot.specimenDeposit2();
                     return false;
