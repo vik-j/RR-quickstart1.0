b1816619808babd7c6675b3ce6b0c5d8bd11dace
vik-j <114836259+vik-j@users.noreply.github.com>
2025-02-13 19:25:01 -0800

    autostic

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/TuningOpModes.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/TuningOpModes.java
@@ -162,7 +162,7 @@ public final class TuningOpModes {
 
 //        manager.register(metaForClass(ManualFeedbackTuner.class), ManualFeedbackTuner.class);
 //        manager.register(metaForClass(SplineTest.class), SplineTest.class);
-//        manager.register(metaForClass(LocalizationTest.class), LocalizationTest.class);
+
 
         FtcDashboard.getInstance().withConfigRoot(configRoot -> {
             for (Class<?> c : Arrays.asList(
